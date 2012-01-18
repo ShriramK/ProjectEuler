0babdebf94113d75efb50af67afb1bf7a0a5eefe
unknown <kshriram18@gmail.com>
2012-01-19 05:23:54 +0530

    Adding solution for Problem 3

---
+++ Problem3.py
@@ -0,0 +1,22 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+
+#What is the largest prime factor of the number 600851475143 ?
+#not more that sqrt or **.5 of 6000851475143
+
+def problemThree():
+	primes = []
+	num = 600851475143
+	temp = num
+	div = 2
+	while num != 1:
+		while num % div == 0:
+			primes.append(div)
+			num = num/div
+		if div == 2:
+			div = 3
+		else:
+			div += 2 #only account for odd numbers
+	print primes[len(primes)-1]
+	print primes
+problemThree()
+#6857
