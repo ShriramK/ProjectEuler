2bd16c522c73fe0c13337549acdaf1418b7b0ee5
unknown <kshriram18@gmail.com>
2012-01-24 20:32:06 +0530

    edited/added relevant code for obtaining solution

--- Problem8.py
+++ Problem8.py
@@ -1,12 +1,67 @@
 #Find the greatest product of five consecutive digits in the 1000-digit number.
 def problemEight():
 	num = str( 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450)
-	product = 1
 	max = 0
+	pos = 0
+	substr = ""
+	while pos < len(num): #find the first five with no zero
+		substr = num[pos:pos+5]	
+		if '0' in substr:
+			pos += substr.find('0')+1			
+		else:
+			break
+
+	sum = findSum(substr)
+	if sum > max:
+		max = sum
+		ans = substr
+		
+	pos += 1
+	
+	while pos < len(num):
+		if num[pos] != '0':
+			sum += int(num[pos])-int(num[pos-5])
+			print ' sum' , sum,
+			substr = substr[1:-1] + num[pos]
+			if sum > max:
+				max = sum
+				ans = substr
+		else:
+			pos += 1
+			while pos+5 <= len(num): #find the first five with no zeroes
+				substr = num[pos:pos+5]
+				if '0' in substr:
+					pos += substr.find('0')+1		
+				else:
+					sum = findSum(substr)
+					if sum > max:
+						max = sum
+						ans = substr
+					break			
+		pos += 1
+
+	print 'ans ', ans
+	product = 1
+	for digit in ans:
+		product *= int(digit)
+	print product
+	
+def findSum(str):
+	sum = 0
+	for i in str:
+		sum += int(i)
+	return sum
+"""
+	for i in range(0, len(num)):
+		substr = num[i:i+5]
+		if '0' not in substr:
+			
+			
 	for j in range(len(num)-5):
 		for index in range(5):
 			product *= product * int(num[index+4]) / int(num[0+index-1])
 			if max < product:
 				max = product
 	print max
+"""
 problemEight()
