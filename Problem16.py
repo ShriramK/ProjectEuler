5bfc43fba098a304b93be2ad7385495a2c8ab669
unknown <kshriram18@gmail.com>
2012-01-28 18:26:06 +0530

    Solution for problem16

---
+++ Problem16.py
@@ -0,0 +1,20 @@
+#Problem 16
+#03 May 2002
+
+#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+#What is the sum of the digits of the number 2^1000?
+#concept of Arbitrary precision arithmetic
+def problemSixteen():
+	num = 1
+	for i in range(0,1000):
+		num*=2
+	strnum = str(num)
+	sum = 0
+	for digit in strnum:
+		sum += int(digit)
+	print sum
+
+problemSixteen()
+#1366 
+
