4453aff0b10ae1c0b0fb029468a42b5aa344454e
unknown <kshriram18@gmail.com>
2012-01-29 11:08:50 +0530

    Changed name of problem11 to problem13 and added Problem11 file separately

---
+++ Problem13.py
@@ -0,0 +1,27 @@
+#Problem 13
+#22 March 2002
+
+#Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+def problemThirteen():
+	#create a list of 100 strings of length 50
+	ans = ""
+	input = open('input.txt', 'r')
+	li = []
+	for line in input:
+		newline = line
+		li.append(newline[:50])
+		
+	carry = 0
+	for i in range(50 - 1, -1, -1):
+		sum = 0
+		for index in range(0,len(li)):
+			sum += int(li[index][i])
+		sum += carry
+		carry = int(str(sum)[:-1])
+		ans = str(sum)[-1] + ans
+	ans = str(carry) + ans
+	print ans[:10]
+	input.close()
+
+problemThirteen()
+#5537376230
