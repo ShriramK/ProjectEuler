ed84596b4e04687af45041939afd826a8e32621f
Shriram Kunchanapalli <kshriram18@gmail.com>
2017-01-09 13:50:44 +0530

    Uses built-in math module function factorial

--- Problem20.py
+++ Problem20.py
@@ -7,11 +7,10 @@
 #and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 #Find the sum of the digits in the number 100!
+import math
 def problemTwenty():
 	sum =0
-	num = 1
-	for i in range(100,0,-1):
-		num *= i
+	num = math.factorial(100)
 	for digit in str(num):
 		sum += int(digit)
 	print sum
