f5995d97945e656097c673fb1bb578eca2eb1a2f
Shriram Kunchanapalli <kshriram18@gmail.com>
2017-01-09 13:28:13 +0530

    Uses the python built-in library function pow

--- Problem16.py
+++ Problem16.py
@@ -6,9 +6,7 @@
 #What is the sum of the digits of the number 2^1000?
 #concept of Arbitrary precision arithmetic
 def problemSixteen():
-	num = 1
-	for i in range(0,1000):
-		num*=2
+	num = pow(2,1000)
 	sum = 0
 	for digit in str(num):
 		sum += int(digit)
